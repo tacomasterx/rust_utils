77f40b1c2659833d8eab36ebff165b7823799db0
Tomas Carrillo <tacoman8918@gmail.com>
2023-12-22 21:41:14 -0600

    Improved countdown and countup, added Timer::from_secs()

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,17 @@ pub mod timer {
             self.seconds + self.minutes * 60 + self.hours * 3600
         }
 
+        pub fn from_secs(secs: u32) -> Timer {
+            let hours = secs / 3600;
+            let seconds = secs % 60;
+            let minutes = (secs - hours * 3600) % 60;
+            Timer{
+                seconds,
+                minutes,
+                hours,
+            }
+        }
+
         pub fn new(hours: u32, minutes: u32, seconds: u32) -> Timer{
             Timer{
                 seconds,
@@ -65,22 +76,46 @@ pub mod timer {
             }
         }
 
+        pub fn countup(&mut self, start: &Instant) {
+            loop {
+                let elapsed_time = start.elapsed().as_micros();
+                if  elapsed_time >= 1_000_000 {
+                    if self.seconds < 60 {
+                        self.seconds += 1;
+                        break
+                    }
+                    if self.minutes < 60 {
+                        self.minutes += 1;
+                        self.seconds = 00;
+                        break
+                    }
+                    self.hours += 1;
+                    self.minutes = 00;
+                    self.seconds = 00;
+                    break
+                }
+            }
+        }
+
         pub fn countdown(&mut self, start: &Instant) {
             loop {
                 let elapsed_time = start.elapsed().as_micros();
-                if elapsed_time >= 1_000_000 {
-                    let value = self.get_seconds() - 1;
-                    if value >= 60 {
-                        self.seconds = value % 60;
-                        if value / 60 >= 60 {
-                            self.hours = value / 60 / 60;
-                            self.minutes = (value / 60) % 60;
-                            break
-                        }
-                        self.minutes = value / 60;
+                if  elapsed_time >= 1_000_000 {
+                    if self.seconds > 0 {
+                        self.seconds -= 1;
                         break
                     }
-                    self.seconds = value;
+                    if self.minutes > 0 {
+                        self.minutes -= 1;
+                        self.seconds = 59;
+                        break
+                    }
+                    if self.hours > 0 {
+                        self.hours -= 1;
+                        self.minutes = 59;
+                        self.seconds = 59;
+                        break
+                    }
                     break
                 }
             }
@@ -122,11 +157,27 @@ mod tests {
     }
 
     #[test]
+    fn from_secs_works() {
+        let mut timer = timer::Timer::from_secs(3661);
+        let origin = Instant::now();
+        timer.countup(&origin);
+        assert!(timer.get_seconds() == 3662)
+    }
+
+    #[test]
     fn tic_works() {
         let mut timer = timer::Timer::new(0,0,0);
         let origin = Instant::now();
         timer.tic(&origin);
         assert!(timer.get_seconds() == 1)
+    }
+
+    #[test]
+    fn countup_works() {
+        let mut timer = timer::Timer::new(1,1,1);
+        let origin = Instant::now();
+        timer.countup(&origin);
+        assert!(timer.get_seconds() == 3662)
     }
 
     #[test]
